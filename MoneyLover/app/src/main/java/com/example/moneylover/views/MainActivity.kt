702bc831f1c622093c4b36f6d9677e6b28ccdb25
Varun Wani <varunwani22@gmail.com>
2021-03-09 15:48:41 +0530

    svgs of bottom navigation project

--- MoneyLover/app/src/main/java/com/example/moneylover/views/MainActivity.kt
+++ MoneyLover/app/src/main/java/com/example/moneylover/views/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.moneylover.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.moneylover.R
+import com.example.moneylover.views.bottomnavigation.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(Fragment())
+        fragmentManager = supportFragmentManager
+
+        val bottomNavigationView =
+            findViewById<View>(R.id.idForBottomNavigation) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+    }
+
+    private fun loadFragment(navigationFragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(
+            R.id.rlContainerForBottomNavigation,
+            navigationFragment
+        ).commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var navigationFragment: Fragment? = null
+        when (item.itemId) {
+            R.id.navigationTransactions -> {
+                navigationFragment = TransactionFragment()
+            }
+            R.id.navigationReport -> {
+                navigationFragment = ReportFragment()
+            }
+            R.id.navigationAddTransaction -> {
+                navigationFragment = AddTransactionFragment()
+            }
+            R.id.navigationPlanning -> {
+                navigationFragment = PlanningFragment()
+            }
+            R.id.navigationAccount -> {
+                navigationFragment = AccountFragment()
+            }
+
+        }
+        if (navigationFragment != null) {
+            loadFragment(navigationFragment)
+        }
+        return true
     }
 }
