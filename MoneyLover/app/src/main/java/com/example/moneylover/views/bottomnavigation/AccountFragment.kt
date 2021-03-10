42c2a2a94b2c2a8efe707a2e1a18347aa6420c83
Kundan Kumar <kundan211097@gmail.com>
2021-03-10 21:48:21 +0530

    Sign out button dialog and settings activity

--- MoneyLover/app/src/main/java/com/example/moneylover/views/bottomnavigation/AccountFragment.kt
+++ MoneyLover/app/src/main/java/com/example/moneylover/views/bottomnavigation/AccountFragment.kt
@@ -1,6 +1,8 @@
 package com.example.moneylover.views.bottomnavigation
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -8,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+
 import androidx.fragment.app.Fragment
 import com.example.moneylover.R
 import com.example.moneylover.views.SettingsActivity
@@ -45,11 +48,27 @@ class AccountFragment : Fragment() {
         var arr=user!!.email!!.split('@')
         tvName.text=arr[0]
         tvEmail.text=user!!.email.toString()
-        tvSignOut.setOnClickListener {
+        btnSignOut.setOnClickListener {
+            val builder=AlertDialog.Builder(requireContext())
+            builder.setTitle("Wait a sec...")
+            builder.setMessage("Siging out from the device?\n\nYour data on this device will be deleted, " +
+                    "but you can always get them back once you sign in again.")
+            builder.setPositiveButton("SIGN OUT"){dialog: DialogInterface?, which: Int ->
                 auth!!.signOut()
                 val intent = Intent(activity, SignInActivity::class.java)
                 startActivity(intent)
-                 requireActivity().finish()
+                requireActivity().finish()
+            }
+            builder.setNegativeButton("CANCEL"){dialogInterface, which ->
+
+            }
+            // Create the AlertDialog
+            val alertDialog: AlertDialog = builder.create()
+            // Set other dialog properties
+            alertDialog.setCancelable(true)
+            alertDialog.show()
+
+
 
         }
         llSettings.setOnClickListener {
