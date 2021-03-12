7a3d84ffebc3e3db8030f2e9b0a0709035bec915
Dhanashree Rane <dhanashreerane18@gmail.com>
2021-03-12 17:40:13 +0530

    About page updated

--- MoneyLover/app/src/main/java/com/example/moneylover/views/bottomnavigation/AccountFragment.kt
+++ MoneyLover/app/src/main/java/com/example/moneylover/views/bottomnavigation/AccountFragment.kt
@@ -1,11 +1,14 @@
 package com.example.moneylover.views.bottomnavigation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.moneylover.R
+import com.example.moneylover.views.AboutActivity
+import kotlinx.android.synthetic.main.fragment_account.*
 
 
 class AccountFragment : Fragment() {
@@ -26,4 +29,11 @@ class AccountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        llAboutUs.setOnClickListener{
+            val intent = Intent (activity,AboutActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
