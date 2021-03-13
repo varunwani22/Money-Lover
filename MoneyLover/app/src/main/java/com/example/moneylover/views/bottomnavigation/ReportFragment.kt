222d7f1587ebafd6e6362ade669188c908fe0462
Kundan Kumar <kundan211097@gmail.com>
2021-03-13 11:02:49 +0530

    final updated

--- MoneyLover/app/src/main/java/com/example/moneylover/views/bottomnavigation/ReportFragment.kt
+++ MoneyLover/app/src/main/java/com/example/moneylover/views/bottomnavigation/ReportFragment.kt
@@ -5,11 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.moneylover.R
+import com.example.moneylover.data.localtransaction.TransactionDatabase
+import com.example.moneylover.repository.TransactionRepository
+import com.example.moneylover.viewmodels.TransactionViewModel
+import com.example.moneylover.viewmodels.TransactionViewModelFactory
+import com.example.moneylover.views.recyclerviews.TransactionAdapter
+import kotlinx.android.synthetic.main.fragment_today.*
 
 
 class ReportFragment : Fragment() {
-
+    private lateinit var viewModel: TransactionViewModel
+    private val transactionDao by lazy {
+        val roomDatabase = TransactionDatabase.getDatabase(requireContext())
+        roomDatabase.getTransactionDao()
+    }
+    val repository by lazy {
+        TransactionRepository(transactionDao)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +43,13 @@ class ReportFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModelFactory = TransactionViewModelFactory(repository)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(TransactionViewModel::class.java)
+
+        viewModel.getTransaction().observe(viewLifecycleOwner, Observer {
+        tvOpenAmountToday.text=it[0].amount.toString()
+        })
 
     }
 }
